c2aaee4a023f9827d38c670967a37bab64b868da
Lance Hart <shipsimfan@hotmail.com>
2024-03-25 15:29:00 -0400

    Added docs

--- src/runner/worker/client.rs
+++ src/runner/worker/client.rs
@@ -1,6 +1,7 @@
 use super::connections::Connections;
 use std::{rc::Rc, sync::Arc};
 
+/// A function which handles a client until an error occurs or a client disconnects
 pub(super) async fn handle_client<
     Protocol: crate::Protocol,
     App: crate::App<Protocol = Protocol>,
