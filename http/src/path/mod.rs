89d103d99e4ad0571d6aadac63a6ee37dacf0da4
Lance Hart <me@lancehart.ca>
2024-07-27 13:09:33 -0400

    Implemented path parsing

--- http/src/path/mod.rs
+++ http/src/path/mod.rs
@@ -7,14 +7,101 @@ pub use query_param::HTTPQueryParam;
 
 /// A parsed HTTP request path
 pub struct HTTPPath<'a> {
-    segments: Vec<Cow<'a, str>>,
+    segments: Vec<Cow<'a, [u8]>>,
     query_params: Vec<HTTPQueryParam<'a>>,
+}
+
+fn from_hexdigit(x: u8) -> u8 {
+    if x.is_ascii_digit() {
+        x - b'0'
+    } else if x.is_ascii_lowercase() {
+        x - b'f' + 10
+    } else {
+        x - b'F' + 10
+    }
+}
+
+fn parse_segment_until<'a, F: Fn(u8) -> bool>(
+    mut i: usize,
+    target: &'a HTTPTarget<'a>,
+    predicate: F,
+) -> (Cow<'a, [u8]>, usize) {
+    let start = i;
+    let mut segment = None;
+
+    while target.len() > i && !predicate(target[i]) {
+        if target[i] == b'%'
+            && target.len() > i + 2
+            && target[i + 1].is_ascii_hexdigit()
+            && target[i + 2].is_ascii_hexdigit()
+        {
+            let low = from_hexdigit(target[i + 1]);
+            let high = from_hexdigit(target[i + 2]);
+
+            if segment.is_none() {
+                let mut new_segment = Vec::with_capacity(start - i);
+                new_segment.copy_from_slice(&target[start..i]);
+                segment = Some(new_segment);
+            }
+
+            segment.as_mut().unwrap().push(low | (high << 4));
+            i += 3;
+            continue;
+        }
+
+        if let Some(segment) = &mut segment {
+            segment.push(target[i]);
+        }
+        i += 1;
+    }
+
+    let segment = match segment {
+        Some(segment) => segment.into(),
+        None => target[start..i].into(),
+    };
+    (segment, i)
 }
 
 impl<'a> HTTPPath<'a> {
     /// Parses `target` into an [`HTTPPath`]
-    pub fn parse(target: &HTTPTarget<'a>) -> Self {
-        todo!()
+    pub fn parse(target: &'a HTTPTarget<'a>) -> Self {
+        if target.len() == 0 {
+            return HTTPPath {
+                segments: Vec::new(),
+                query_params: Vec::new(),
+            };
+        }
+
+        let mut i = 0;
+        let mut leading_slash = target[0] == b'/';
+        let mut segments = Vec::new();
+        while target.len() > i && target[i] != b'?' {
+            if leading_slash {
+                i += 1;
+            }
+
+            let (segment, new_i) = parse_segment_until(i, target, |x| x == b'/' || x == b'?');
+            i = new_i;
+            segments.push(segment);
+
+            leading_slash = true;
+        }
+
+        if target.len() > i && target[i] == b'?' {
+            i += 1;
+        }
+
+        let mut query_params = Vec::new();
+        while target.len() > i {
+            let (query_param, new_i) = HTTPQueryParam::parse(i, target);
+            i = new_i;
+            query_params.push(query_param);
+        }
+
+        HTTPPath {
+            segments,
+            query_params,
+        }
     }
 
     /// Gets the number of segments which make up this path
@@ -23,13 +110,13 @@ impl<'a> HTTPPath<'a> {
     }
 
     /// Gets the segment at index `i`
-    pub fn segment(&self, i: usize) -> &str {
+    pub fn segment(&self, i: usize) -> &[u8] {
         &self.segments[i]
     }
 
     /// Gets an iterator over the segments of this path
-    pub fn segments(&self) -> impl Iterator<Item = &str> {
-        self.segments.iter().map(|str| str.as_ref())
+    pub fn segments(&self) -> impl Iterator<Item = &[u8]> {
+        self.segments.iter().map(|segment| segment.as_ref())
     }
 
     /// Gets the query parameters of the request path
@@ -38,7 +125,7 @@ impl<'a> HTTPPath<'a> {
     }
 
     /// Attempts to get a query parameter with `key`
-    pub fn query_param(&self, key: &str) -> Option<&str> {
+    pub fn query_param(&self, key: &[u8]) -> Option<&[u8]> {
         for query_param in &self.query_params {
             if query_param.key() == key {
                 return Some(query_param.value());
--- http/src/path/query_param.rs
+++ http/src/path/query_param.rs
@@ -1,24 +1,33 @@
-use std::{borrow::Cow, iter::Peekable};
+use super::parse_segment_until;
+use crate::HTTPTarget;
+use std::borrow::Cow;
 
 /// A query parameter passed in an HTTP request
 pub struct HTTPQueryParam<'a> {
-    key: Cow<'a, str>,
-    value: Cow<'a, str>,
+    key: Cow<'a, [u8]>,
+    value: Cow<'a, [u8]>,
 }
 
 impl<'a> HTTPQueryParam<'a> {
     /// Parse an [`HTTPQueryParam`] from `stream`
-    pub(super) fn parse<I: Iterator<Item = u8>>(stream: Peekable<I>) -> Self {
-        todo!()
+    pub(super) fn parse(i: usize, target: &'a HTTPTarget<'a>) -> (Self, usize) {
+        let (key, i) = parse_segment_until(i, target, |x| x == b'=' || x == b'&');
+        let (value, i) = if target[i] == b'=' {
+            parse_segment_until(i, target, |x| x == b'&')
+        } else {
+            (Cow::Borrowed(&[] as &[u8]), i)
+        };
+
+        (HTTPQueryParam { key, value }, i + 1)
     }
 
     /// Gets the key of this query parameter
-    pub fn key(&self) -> &str {
+    pub fn key(&self) -> &[u8] {
         &self.key
     }
 
     /// Gets the value of this query parameter
-    pub fn value(&self) -> &str {
+    pub fn value(&self) -> &[u8] {
         &self.value
     }
 }
